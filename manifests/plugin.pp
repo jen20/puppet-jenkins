b6415fc1d849d239744b6414e50fc757f9dd2853
liamjbennett <lbennett@opentable.com>
2014-05-15 16:38:26 +0100

    adding exec unless condition for plugin

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -74,11 +74,13 @@ define jenkins::plugin(
       cwd        => $plugin_dir,
       require    => [File[$plugin_dir], Package['wget']],
       path       => ['/usr/bin', '/usr/sbin', '/bin'],
+      unless     => "cat ${plugin_dir}/${name}/META-INF/MANIFEST.MF | grep 'Plugin-Version' | awk '{print \$2}' | sed 's/\r//' | xargs -I {} bash -c 'if [ {} = \"${version}\" ]; then exit 0; else exit 1; fi'"
     }
 
     file { "${plugin_dir}/${plugin}" :
       require => Exec["download-${name}"],
       owner   => 'jenkins',
+      group   => 'jenkins',
       mode    => '0644',
       notify  => Service['jenkins'],
     }
